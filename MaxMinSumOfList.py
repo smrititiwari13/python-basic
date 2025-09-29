ba80360d7204c9669765a7dd7864d306db011d7e
smrititiwari13 <smrititiwari1313@gmail.com>
2025-09-29 11:16:39 +0530

    Create MaxMinSumOfList.py

---
+++ MaxMinSumOfList.py
@@ -0,0 +1,6 @@
+my_list = [10, 25, 30, 5, 70, 15]
+
+print("List:", my_list)
+print("Sum of elements:", sum(my_list))
+print("Maximum element:", max(my_list))
+print("Minimum element:", min(my_list))
