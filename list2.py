0df5939a2b865fdece1cb22446432b766e6d18bb
smrititiwari13 <smrititiwari1313@gmail.com>
2025-09-29 11:29:28 +0530

    Create list2.py

---
+++ list2.py
@@ -0,0 +1,30 @@
+my_list = [10, 20, 30, 40, 50]
+print( my_list)
+
+my_list.insert(2, 25)
+print( my_list)
+
+my_list.remove(40)
+print( my_list)
+my_list.pop(2)
+print( my_list)
+
+
+my_list[1] = 15
+print( my_list)
+
+my_list.append(60)
+print( my_list)
+
+
+
+another_list = [70, 80, 90]
+my_list.extend(another_list)
+print( my_list)
+del my_list
+my_list = [10, 20, 30, 40, 50]
+
+my_list.insert(0,5)
+print(my_list)
+
+
